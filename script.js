928a2d5e3deefd7ed5e0c2d9976009c642dba45d
kalyi01 <ksagynbekew@gmail.com>
2023-03-16 16:47:06 +0300

    asdfasdf

---
+++ script.js
@@ -0,0 +1,52 @@
+// Дан массив с числами. Сделайте из него массив, состоящий из квадратов этих чисел.
+// const num = [1, 2, 3, 0, 4, 5, 6]
+// const array = num.map(el => el * el)
+// console.log(array)
+
+// Дан массив с числами. Проверьте то, что все элементы в массиве больше нуля.
+// const numNEW = [1, 2, 3, 0, 4, 5, 6]
+// const array = numNEW.map(el => el > 0 )
+//
+// console.log(array)
+
+// Дан массив с числами. Оставьте в нем только отрицательные числа.
+// const num = [1, 2, -3, 0, 4, 5, -6]
+// const array = num.filter(el => el < 0 )
+// console.log(array)
+
+// Дан массив с числами. Оставьте в нем только четные числа.
+// const num = [1, -2, 3, 0, 4, -5, 6]
+// const array = num.filter(el => el % 2 === 0 )
+// console.log(array)
+
+
+// Дан массив со строками. Оставьте в нем только те строки, длина которых больше 5-ти символов.
+// const str = ['hello', 'sit', 'ipsum', 'javascript', 'python', 'sun', 'even']
+// const str2 = str.filter(el => el.length > 5)
+// console.log(str2)
+
+
+// Дан массив с числами. Проверьте то, что в нем есть отрицательные элементы.
+// const num = [1, 2, -3, 0, 4, -5, 6]
+// const array = num.filter(el => el < 0)
+// if (array.length === num.length){
+//     console.log(false)
+// }else {
+//     console.log(true)
+// }
+
+
+
+// Дан массив с числами. Посчитайте количество отрицательных чисел в этом массиве.
+// const num = [1, 2, -3, 0, -4, 5, -6]
+// const array = num.filter(el => el < 0)
+// console.log(array.length)
+
+// Дан массив с числами. Найдите сумму этих чисел.
+// const num = [1, 2, 3, 33, 4, 5, 6]
+// const array = num.reduce((acc , el) =>{
+//     return acc + el
+// })
+// console.log(array)
+
+
